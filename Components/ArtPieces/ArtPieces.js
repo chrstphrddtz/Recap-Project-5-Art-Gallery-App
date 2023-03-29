18cc68be248317794ade90129c160018dd7fce50
christoph.raddatz <hello@chrstphrddtz.com>
2023-03-29 18:25:53 +0200

    change image size

--- Components/ArtPieces/ArtPieces.js
+++ Components/ArtPieces/ArtPieces.js
@@ -12,19 +12,21 @@ const ListItem = styled.li`
   width: 100px;
 `;
 
-// const StyledImage = styled(Image)`
-// box-shadow: var(--box-shadow-book);
-// ` 
+const StyledImage = styled(Image)`
+box-shadow: var(--box-shadow-book);
+width: 25%;
+height: 20%;
+` 
 
 export default function ArtPieces({pieces}) {
   return (
     <ul>
       {pieces.map((piece) => (
-        <li key={uid}>
-        <Image
+        <li key={uid()}>
+        <StyledImage
           src={piece.imageSource}
-          width={piece.dimensions.width/10}
-          height={piece.dimensions.height/10}
+          width={piece.dimensions.width}
+          height={piece.dimensions.height}
           alt={piece.name}
         />
       </li>
