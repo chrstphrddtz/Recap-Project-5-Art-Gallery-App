d411cbea97d58a0dfb69c7a8971b370833f5e54f
christoph.raddatz <hello@chrstphrddtz.com>
2023-03-29 21:38:57 +0200

    Add Component: ArtPiecePreview + first styling

--- Components/ArtPiecePreview/ArtPiecePreview.js
+++ Components/ArtPiecePreview/ArtPiecePreview.js
@@ -1,7 +1,37 @@
+import Image from "next/image";
+import styled  from "styled-components";
 
 
-export default function ArtPiecePreview({image, title, artist}) {
+const Container = styled.div`
+  /* width: 40%; */
+  margin: 20px;
+  text-align: center;  
+`
+const StyledImage = styled(Image)`
+  width: 40%;
+  height: 40%;
+` 
+
+const ArtworkTitle = styled.h2`
+
+`
+
+const ArtistTitle = styled.h2`
+
+`
 
 
-
+export default function ArtPiecePreview({image, title, artist, width, height}) {
+  return (
+    <Container>
+      <StyledImage
+          src={image}
+          width={width}
+          height={height}
+          alt={title}
+        />
+        <h2>{title}</h2>
+        <h3>{artist}</h3>
+    </Container>
+  )
 }
