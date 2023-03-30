98bf287e687984384a0c2b13fbe92d1c26d8ac53
christoph.raddatz <hello@chrstphrddtz.com>
2023-03-30 15:22:35 +0200

    remove log

--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,7 @@ const url = "https://example-apis.vercel.app/api/art";
 export default function App({ Component, pageProps }) {
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
   const { data, error, isLoading } = useSWR(url, fetcher);
+  console.log(data);
 
   if (error) return <div>failed to load</div>;
   if (isLoading) return <div>loading...</div>;
